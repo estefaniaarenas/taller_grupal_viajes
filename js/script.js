c05ee9d6a2b35bfdecdb9fbef15fc49eaf089e72
estefania <estefaniaaren0@gmail.com>
2022-03-29 00:23:13 -0500

    cambios estefania

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-function formulario (){
+function Enviar (){
    
     let nombre = document.getElementById("inputName").value;
-    let apellido =
+    let apellido = document.getElementById("inputLast").value;
+    let email = document.getElementById("inputEmail").value;
+    let telefono = document.getElementById("inputPhone").value;
+
+    Swal.fire({
+        position: 'top',
+        icon: 'success',
+        title: 'Tú información ha sido enviada',
+        showConfirmButton: false,
+        timer: 3500,
+        text: `El usuario envio los siguientes datos: \n nombre: ${nombre} \n apellido: ${apellido} \n email: ${email} \n phone: ${telefono}`
+      })
+      document.getElementById(`inputName`).value = "";
+      document.getElementById(`inputLast`).value = "";
+      document.getElementById(`inputEmail`).value = "";
+      document.getElementById(`inputPhone`).value = "";
 }
